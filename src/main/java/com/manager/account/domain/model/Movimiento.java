2fc6d948eb2e5ff27696ee25bb07a931b64c25c7
Diego Velez <diego8207@gmail.com>
2023-02-18 16:50:02 -0500

    add unit test EstadoCuentaControllerTest

--- src/main/java/com/manager/account/domain/model/Movimiento.java
+++ src/main/java/com/manager/account/domain/model/Movimiento.java
@@ -2,12 +2,14 @@ package com.manager.account.domain.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigInteger;
 import java.util.Date;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Movimiento {
