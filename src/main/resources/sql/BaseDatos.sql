d1e94d5203367ae864bc81ba8f11480f39f1cb00
Diego Velez <diego8207@gmail.com>
2023-02-19 11:02:19 -0500

    fix script database

--- src/main/resources/sql/BaseDatos.sql
+++ src/main/resources/sql/BaseDatos.sql
@@ -1,138 +1,66 @@
-CREATE DATABASE  IF NOT EXISTS `account_manager` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `account_manager`;
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
---
--- Host: localhost    Database: account_manager
--- ------------------------------------------------------
--- Server version	8.0.32
+CREATE DATABASE  IF NOT EXISTS account_manager;
+USE account_manager;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+DROP TABLE IF EXISTS movimiento;
+DROP TABLE IF EXISTS cuenta;
+DROP TABLE IF EXISTS cliente;
+DROP TABLE IF EXISTS persona;
 --
--- Table structure for table `cliente`
+-- Table structure for table persona
 --
 
-DROP TABLE IF EXISTS `cliente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cliente` (
-  `contrasena` varchar(10) NOT NULL,
-  `estado` varchar(10) NOT NULL,
-  `identificacion` varchar(20) NOT NULL,
-  KEY `identificacion_fk_idx` (`identificacion`),
-  CONSTRAINT `identificacion_fk` FOREIGN KEY (`identificacion`) REFERENCES `persona` (`identificacion`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE persona (
+  identificacion varchar(20) NOT NULL,
+  nombre varchar(45) NOT NULL,
+  genero varchar(10) NOT NULL,
+  edad int NOT NULL,
+  direccion varchar(60) NOT NULL,
+  telefono varchar(20) NOT NULL,
+  PRIMARY KEY (identificacion)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `cliente`
+-- Table structure for table cliente
 --
 
-LOCK TABLES `cliente` WRITE;
-/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `cuenta`
---
-
-DROP TABLE IF EXISTS `cuenta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cuenta` (
-  `numero` varchar(20) NOT NULL,
-  `tipo_cuenta` varchar(10) NOT NULL,
-  `saldo` bigint NOT NULL DEFAULT '0',
-  `estado` varchar(5) NOT NULL DEFAULT 'true',
-  `cliente_id` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`numero`),
-  KEY `cliiente_id_fk_idx` (`cliente_id`),
-  CONSTRAINT `cliiente_id_fk` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`identificacion`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE cliente (
+  contrasena varchar(10) NOT NULL,
+  estado varchar(10) NOT NULL,
+  identificacion varchar(20) NOT NULL,
+  KEY identificacion_fk_idx (identificacion),
+  CONSTRAINT identificacion_fk FOREIGN KEY (identificacion) REFERENCES persona (identificacion) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `cuenta`
+-- Table structure for table cuenta
 --
 
-LOCK TABLES `cuenta` WRITE;
-/*!40000 ALTER TABLE `cuenta` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cuenta` ENABLE KEYS */;
-UNLOCK TABLES;
+
+CREATE TABLE cuenta (
+  numero varchar(20) NOT NULL,
+  tipo_cuenta varchar(10) NOT NULL,
+  saldo bigint NOT NULL DEFAULT '0',
+  estado varchar(5) NOT NULL DEFAULT 'true',
+  cliente_id varchar(20) DEFAULT NULL,
+  PRIMARY KEY (numero),
+  KEY cliiente_id_fk_idx (cliente_id),
+  CONSTRAINT cliiente_id_fk FOREIGN KEY (cliente_id) REFERENCES cliente (identificacion) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Table structure for table `movimiento`
+-- Table structure for table movimiento
 --
 
-DROP TABLE IF EXISTS `movimiento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `movimiento` (
-  `movimiento_id` int NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
-  `tipo_movimiento` varchar(15) NOT NULL,
-  `valor` bigint NOT NULL,
-  `saldo` bigint NOT NULL,
-  `cuenta_id` varchar(20) NOT NULL,
-  PRIMARY KEY (`movimiento_id`),
-  KEY `cuenta_fk_idx` (`cuenta_id`),
-  CONSTRAINT `cuenta_fk` FOREIGN KEY (`cuenta_id`) REFERENCES `cuenta` (`numero`)
+
+CREATE TABLE movimiento (
+  movimiento_id int NOT NULL AUTO_INCREMENT,
+  fecha date NOT NULL,
+  tipo_movimiento varchar(15) NOT NULL,
+  valor bigint NOT NULL,
+  saldo bigint NOT NULL,
+  cuenta_id varchar(20) NOT NULL,
+  PRIMARY KEY (movimiento_id),
+  KEY cuenta_fk_idx (cuenta_id),
+  CONSTRAINT cuenta_fk FOREIGN KEY (cuenta_id) REFERENCES cuenta (numero)
 ) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `movimiento`
---
-
-LOCK TABLES `movimiento` WRITE;
-/*!40000 ALTER TABLE `movimiento` DISABLE KEYS */;
-/*!40000 ALTER TABLE `movimiento` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `persona`
---
-
-DROP TABLE IF EXISTS `persona`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `persona` (
-  `identificacion` varchar(20) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `genero` varchar(10) NOT NULL,
-  `edad` int NOT NULL,
-  `direccion` varchar(60) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  PRIMARY KEY (`identificacion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `persona`
---
-
-LOCK TABLES `persona` WRITE;
-/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-02-16 20:13:00
